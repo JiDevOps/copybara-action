dcd858b9a77778b9b048c3e6e33897c20befe26c
Ji <jjeonie@gmail.com>
2020-08-30 11:39:43 +1200

    Create action.yml, entrypoint and Dockerfile for running as real github action

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM alpine:latest
+
+RUN apk get --no-cache \ 
+  docker-cli
+  git
+
+COPY "entrypoint.sh" "/entrypoint.sh"
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+
+mkdir ~/.ssh
+echo $INPUT_SSH_KEY > ~/.ssh/id_rsa
+echo $INPUT_KNOWN_HOSTS > ~/.ssh/known_hosts
+
+git config --global user.name $INPUT_GIT_NAME
+git config --global user.email $INPUT_GIT_EMAIL
+
+docker pull sharelatex/copybara:latest
+
+cd $INPUT_PATH && docker run -v ~/.ssh:/root/.ssh -v ~/.gitconfig:/root/.gitconfig -v "$(pwd)":/usr/src/app -i sharelatex/copybara copybara
